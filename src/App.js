d4811dd762c868013bd5740dca49a09ed71d4625
dwaynediddy <burrows.dwayne@icloud.com>
2023-07-31 13:59:35 +0200

    added some basic styling fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Reader from './components/Reader'
 
 function App() {
+  const originalSentence =
+    'this is the sentence which should be bold in first characters making it quicker to read, perhaps bionic';
+
   return (
     <div className="App">
-
+      <h1>Bionic Reader App</h1>
+      <Reader sentence={originalSentence} />
     </div>
   );
 }
--- src/components/Reader.js
+++ src/components/Reader.js
@@ -1,32 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 const Reader = (props) => {
-    let words = props.sentence.split(' ')
-    let modifiedWords = []
+  let words = props.sentence.split(' ')
+  let modifiedWords = []
 
-    for(let i = 0; i < words.length; i++) {
-        let word = words[i]
-        let modifiedWord = ''
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i]
+    let modifiedWord = ''
 
-        for(let j = 0; i < word.length; j++) {
-            let character = word[j]
-            let halfLength = Math.ceil(word.length / 2)
+    for (let j = 0; j < word.length; j++) {
+      let character = word[j]
+      let halfLength = Math.ceil(word.length / 2)
 
-            if(j < halfLength) {
-                modified += '<b>' + character + '</b>'
-            } else {
-                modifiedWord += character
-            }
-        }
-        modifiedWords.push(modifiedWord)
+      if (j < halfLength) {
+        modifiedWord += '<b>' + character + '</b>'
+      } else {
+        modifiedWord += character
+      }
     }
 
-    const modifiedSentence = modifiedWords.join(' ')
+    modifiedWords.push(modifiedWord)
+  }
+
+  const modifiedSentence = modifiedWords.join(' ')
+
   return (
-    <div>
-        <p> dangerouslySetInnerHTML={{__html: modifiedSentence}}</p>
+    <div className='reader-container'>
+      <p dangerouslySetInnerHTML={{ __html: modifiedSentence }} />
     </div>
   )
 }
 
-export default Reader
+export default Reader;
