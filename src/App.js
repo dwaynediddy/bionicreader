e989482b33744e02625d486bdeda6372b8d8f731
dwaynediddy <burrows.dwayne@icloud.com>
2023-08-03 16:03:23 +0200

    added dropdown for books

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import OpenLibrary from './components/OpenLibrary'
 import Reader from './components/Reader'
 
 function App() {
-  const originalSentence =
-    'this is the sentence which should be bold in first characters making it quicker to read, perhaps bionic';
-
   return (
     <div className="App">
       <h1>Bionic Reader App</h1>
-      {/* <Reader sentence={originalSentence} /> */}
       <OpenLibrary />
+      {/* <Reader sentence /> */}
     </div>
   );
 }
--- src/components/OpenLibrary.js
+++ src/components/OpenLibrary.js
@@ -1,56 +1,61 @@
 import React, { useState } from 'react';
+import './library.css'
 
 const OpenLibrary = () => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
-  
-    const handleInputChange = (event) => {
-      setSearchQuery(event.target.value);
-    };
-  
-    const searchBooks = () => {
-      const apiUrl = `https://openlibrary.org/search.json?q=${encodeURIComponent(
-        searchQuery
-      )}`;
-  
-      fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-          setSearchResults(data.docs || []);
-        })
-        .catch((error) => {
-          console.error('Error fetching data:', error);
-          setSearchResults([]);
-        });
-    };
-  
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Enter book title"
-          value={searchQuery}
-          onChange={handleInputChange}
-        />
-        <button onClick={searchBooks}>Search Books</button>
-  
-        {searchResults.length > 0 && (
-          <div>
-            <h2>Search Results</h2>
-            <ul>
-              {searchResults.map((book) => (
-                <li key={book.key}>
-                  <p>Title: {book.title}</p>
-                  <p>Author(s): {book.author_name?.join(', ') || 'N/A'}</p>
-                  {/* Display more book information as needed */}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
-    );
-  };
-  
+  const [searchQuery, setSearchQuery] = useState('');
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
 
-export default OpenLibrary
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const searchBooks = () => {
+    const apiUrl = `https://openlibrary.org/search.json?q=${encodeURIComponent(
+      searchQuery
+    )}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchResults(data.docs || []);
+        setDropdownOpen(true); // Open the dropdown
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setSearchResults([]);
+      });
+  };
+
+  const handleSelectBook = (book) => {
+    // Placeholder function to handle book selection
+    console.log('Selected book:', book);
+    setDropdownOpen(false); // Close the dropdown
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter book title"
+        value={searchQuery}
+        onChange={handleInputChange}
+      />
+      <button onClick={searchBooks}>Search Books</button>
+
+      {dropdownOpen && (
+        <div className="dropdown">
+          <ul>
+            {searchResults.map((book) => (
+              <li key={book.key} onClick={() => handleSelectBook(book)}>
+                {book.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OpenLibrary;
